57c2de6f9fe1807fb6a5205d076ba5252acd1ebd
MajoA <majoaguero20@hotmail.comgit config --global user.name MajoAgit commit -m 'primer commit del proyecto de la panaderia''git config --global user.email majoaguero20@hotmail.com>
2022-12-15 20:32:17 -0300

    instale react bootstrap, react rout y db json

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Administrador from "./components/views/Administrador"
+import Administrador from "./components/views/Administrador";
+import Inicio from "./components/views/Inicio";
 function App() {
   return (
    <BrowserRouter>
    <Routes>
     <Route exact path="/Administrador" element={<Administrador></Administrador>}></Route>
+    <Route exact path="/inicio" element={<Inicio></Inicio>}></Route>
    </Routes>
    </BrowserRouter>
   );
---
+++ src/components/views/Inicio.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inicio = () => {
+    return (
+        <div>
+            <h1>HOLA MAJO HERMOSA</h1>
+        </div>
+    );
+};
+
+export default Inicio;
