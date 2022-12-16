548f0b8ffee7496a80bd1b85d2b4d2e860824454
MajoA <majoaguero20@hotmail.comgit config --global user.name MajoAgit commit -m 'primer commit del proyecto de la panaderia''git config --global user.email majoaguero20@hotmail.com>
2022-12-15 22:15:16 -0300

    se creo el nav

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Administrador from "./components/views/Administrador";
 import Inicio from "./components/views/Inicio";
+import Menu from "./components/common/Menu"
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
    <BrowserRouter>
    <Routes>
+    <Route exact path="/Menu" element={<Menu></Menu>}></Route>
     <Route exact path="/Administrador" element={<Administrador></Administrador>}></Route>
     <Route exact path="/inicio" element={<Inicio></Inicio>}></Route>
    </Routes>
